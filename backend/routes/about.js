c794ffe171ab2f024b2e872ab75c585bae82a0a0
stewartrowley <stewy.rowley@gmail.com>
2022-10-14 17:09:17 -0600

    updated the code a little

--- backend/routes/about.js
+++ backend/routes/about.js
@@ -1,13 +1,10 @@
 const routes = require('express').Router();
 
 const aboutController = require('../controllers/about');
-const { about } = require('../models');
-
-console.log(aboutController);
 
 routes.get('/', aboutController.getAboutAll);
 
-routes.get('/:aboutId', aboutController.getAbout);
+routes.get('/:_id', aboutController.getAbout);
 
 routes.post('/', aboutController.createAbout);
 
